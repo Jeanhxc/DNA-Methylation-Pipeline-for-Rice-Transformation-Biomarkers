c6ade160cef463ac6fbbc8330923f563bfaf8033
Shiang-Chin Huang <125529707+Jeanhxc@users.noreply.github.com>
2026-05-23 13:00:54 +0800

    Remove unnecessary comments and clean up code

---
+++ Indentify_DMR.r
@@ -0,0 +1,22 @@
+
+callusCG= read.table("TNG67_TrUn_callus_common_region_CG.txt",sep="\t",header=T)
+cgtrun_selected_columns <- c("Chr","start","end","T17","T32","T65","C02","C04")
+CGtrun <- callusCG[, cgtrun_selected_columns]
+
+cutoff_CG=0.25
+
+CGtrun[[4]] = CGtrun[[4]] + 0.0001
+CGtrun[[7]] = CGtrun[[7]] + 0.0001
+
+CGtrun$p_value=apply(CGtrun[,4:8],1,function(x){t.test(x[1:3], x[4:5], alternative="two.sided")$p.value})
+CGtrun[[4]]=CGtrun[[4]]-0.0001
+CGtrun[[7]]=CGtrun[[7]]-0.0001
+
+CGtrun$Un_mean=apply(CGtrun[,7:8],1, mean)
+CGtrun$Tr_mean=apply(CGtrun[,4:6],1, mean)
+CGtrun$delta=CGtrun$Tr_mean-CGtrun$Un_mean
+
+CG_trun=CGtrun[abs(CGtrun$delta)>=cutoff_CG&(CGtrun$p_value< 0.05),]
+CG_trun$type <- ifelse (CG_trun$delta>0,"hyper","hypo")
+
+
